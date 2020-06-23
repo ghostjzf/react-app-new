2799ba74c7fb444ae8f59768a2163dff6cee585b
jiangzhifeng <jiangzhifeng666@163.com>
2020-06-23 22:58:10 +0800

    feat(clean): add clean-webpack-plugin

--- scripts/config/webpack.prod.config.js
+++ scripts/config/webpack.prod.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
 const webpackCommon = require("./webpack.common.js");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(webpackCommon, {
   mode: "production",
   entry: path.join(__dirname, "../../", "src/index.js"),
   output: { path: path.resolve(__dirname, "../../dist") },
+  plugins: [new CleanWebpackPlugin()],
 });
